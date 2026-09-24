09f0371b297d3aa8b304eb8104c200a9d6039ea9
Mamudoon <arthur.t.morita@gmail.com>
2026-09-24 18:07:59 -0300

    Adicionei os arquivos para o trabalho 9

---
+++ 09-vetor/dequef.c
@@ -0,0 +1,136 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dequef.h"
+
+
+/**
+   Create an empty deque of floats.
+
+   capacity is both the initial and minimum capacity.
+   factor is the resizing factor, larger than 1.0.
+
+   On success it returns the address of a new dequef.
+   On failure it returns NULL.
+**/
+dequef* df_alloc(long capacity, double factor) {
+   dequef* D = malloc(sizeof(dequef));
+   if (D == NULL) {
+      return NULL;
+   }
+   D->first = 0;
+   D->size = 0;
+   D->cap = capacity;
+   D->mincap = D->cap;
+   D->factor = factor;
+   D->data = malloc(cap*sizeof(float));
+   if (D->data == NULL) {
+      return NULL;
+   }
+
+}
+
+/**
+  Release a dequef and its data.
+**/
+void df_free(dequef* D) {
+}
+
+
+
+/**
+   The size of the deque.
+**/
+long df_size(dequef* D) {
+}
+
+
+
+/**
+   Add x to the end of D.
+
+   If the array is full, it first tries to increase the array size to
+   capacity*factor.
+
+   On success it returns 1.
+   If attempting to resize the array fails then it returns 0 and D remains unchanged.
+**/
+int df_push(dequef* D, float x) {
+}
+
+
+
+/**
+   Remove a float from the end of D and return it.
+
+   If the deque has capacity/(factor^2) it tries to reduce the array size to
+   capacity/factor.  If capacity/factor is smaller than the minimum capacity,
+   the minimum capacity is used instead.  If it is not possible to resize, then
+   the array size remains unchanged.
+
+   It returns the float removed from D.
+   What happens if D is empty before the call?
+**/
+float df_pop(dequef* D) {
+}
+
+
+
+/**
+   Add x to the beginning of D.
+
+   If the array is full, it first tries to increase the array size to
+   capacity*factor.
+
+   On success it returns 1.
+   If attempting to resize the array fails then it returns 0 and D remains unchanged.
+**/
+int df_inject(dequef* D, float x) {
+}
+
+
+
+/**
+   Remove a float from the beginning of D and return it.
+
+   If the deque has capacity/(factor^2) elements, this function tries to reduce
+   the array size to capacity/factor.  If capacity/factor is smaller than the
+   minimum capacity, the minimum capacity is used instead.
+
+   If it is not possible to resize, then the array size remains unchanged.
+
+   It returns the float removed from D.
+   What happens if D is empty before the call?
+**/
+float df_eject(dequef* D) {
+}
+
+
+
+/**
+   Return D[i].
+
+   If i is not in [0,|D|-1]] what happens then?
+**/
+float df_get(dequef* D, long i) {
+}
+
+
+
+/**
+   Set D[i] to x.
+
+   If i is not in [0,|D|-1]] what happens then?
+**/
+void df_set(dequef* D, long i, float x) {
+}
+
+
+
+/**
+   Print the elements of D in a single line.
+**/
+void df_print(dequef* D) {
+}
